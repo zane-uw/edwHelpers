e1fbe8b0570c1fc6f863119396062aa2197c6441
zane-uw <zane@uw.edu>
2018-05-16 11:33:29 -0700

    add more @export

--- R/txt funs.R
+++ R/txt funs.R
@@ -1,11 +1,12 @@
 #' Create quoted character vectors
 #'
 #' @param ... text to be concatenated.
-#' @export
 #' @return A quoted character vector.
+#'
 #' @examples
 #' Cs(some, pig)
-
+#'
+#' @export Cs
 Cs <- function(...) {as.character(sys.call())[-1]}
 
 
@@ -14,10 +15,11 @@ Cs <- function(...) {as.character(sys.call())[-1]}
 #'
 #' @param x A vector of strings, typically the result of names(...).
 #' @return A character vector in r-ish format, i.e. lowercase text with words separated by '.'.
-#' @export
+#'
 #' @examples
 #' fix.sdb.names(c("SDBSrcSysKey", "QuarterlyGPA", "AcademicYear"))
-
+#'
+#' @export fix.sdb.names
 fix.sdb.names <- function(x = "EDWSampleVarName"){
   # convert UpperStyle edw/sdb names to r style
   tolower(gsub("([[:lower:]])([[:upper:]])", "\\1.\\2", x, fixed = F))
